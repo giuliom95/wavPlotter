a4b6676a9e871056c1e91bc378077d24b927b9ca
giuliom95 <giuliomartella1995@gmail.com>
2015-12-06 18:24:11 +0100

    Added interactivity

--- wavPlotter.c
+++ wavPlotter.c
@@ -3,8 +3,10 @@
 #include "SDL.h"
 
 //The window initial dimensions
-#define SCREEN_WIDTH 1600
+#define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
+#define SCROLL_SPEED 100
 
 //These two define the axis ratio of the plot.
 #define WIDTH 1
@@ -13,27 +15,74 @@
 //The lenght(in bytes) of the header of a standard WAV file.
 #define WAV_HEADER 44
 
+//The two channels of a single file.
+int16_t* left;
+int16_t* right;
+
+//The number of total 16-bit samples that compose a single channel.
+long total_samples;
+
+//An iterator. It will be used a lot of times, so I've declared it here
+// once for all.
+int i;
+
+long position;
+unsigned int width;
+
+SDL_Event e;
+
+void refresh_plot() {
+	//Cleans the screen
+	glClear( GL_COLOR_BUFFER_BIT );
+	
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	glOrtho( position, screenWidth + position, screenHeight, 0.0, 1.0, -1.0 );
+	
+	//Plots the time axis.
+	glBegin( GL_LINES );
+		glColor3ub( 150, 150, 150 );
+		glVertex2f( position, screenHeight / 2 );
+		glVertex2f( screenWidth + position, screenHeight / 2 );
+	glEnd();
+	
+	//NOTE: The FFT algorithm which I've talked about here: 
+	// http://giuliom95.tumblr.com/post/134427522354/
+	// will be implemented in those two next cycles.
+	
+	//Plots the left channel in red. 
+	glBegin( GL_LINE_STRIP );
+		glColor3ub( 255, 100, 100 );
+		for( i = 0; i < total_samples; i+=width ) {
+			glVertex2f( i, screenHeight / 2 + left[i/width] * DEPTH );
+		} 	
+	glEnd();
+
+	//Plots the right channel in green.
+	glBegin( GL_LINE_STRIP );
+		glColor3ub( 100, 255, 100 );
+		for( i = 0; i < total_samples; i+=width ) {
+			glVertex2f( i, screenHeight / 2 + right[i/width] * DEPTH );
+		}	
+	glEnd();	
+	
+	SDL_GL_SwapWindow( gWindow );
+}
+
+
 int main() {
-	
-	//The two channels of a single file.
-	int16_t* left;
-	int16_t* right;
-	
-	//The number of total 16-bit samples that compose a single channel.
-	long total_samples;
-	
-	//An iterator. It will be used a lot of times, so I've declared it here
-	// once for all.
-	int i;
 	
 	//The pointer to the wav file
 	FILE* input_file;
 	
-	//Those variables are needed to make the OpenGL and SDL libraries work. 
+	//These variables are needed to make the OpenGL and SDL libraries work. 
 	gWindow = NULL;
 	gContext = NULL;
 	screenWidth = SCREEN_WIDTH;
 	screenHeight = SCREEN_HEIGHT;
+	
+	position = 0;
+	width = WIDTH;
 	
 	//Creates the window and the OpenGL contex.
 	SDL_init();
@@ -49,9 +98,7 @@ int main() {
 	// so, in order to get the samples, it must be divided by 4, because the channels
 	// are two and every samble is made up by 2 bytes (16-bit). 
 	total_samples = ( ftell( input_file ) - WAV_HEADER ) / 4;
-	
-	printf( "%i\n", total_samples );
-	
+
 	//Brings back the cursor to the head of the file.
 	fseek( input_file, 0, 0 );
 	
@@ -82,56 +129,41 @@ int main() {
 		right[i] = (uint8_t)fgetc( input_file ) + (uint16_t)( fgetc( input_file ) << 8 );		
 	}
 	
-	//And now the plotting part.
-	long i2;
-	for (i2 = 0; i2 < total_samples / 500; i2++) {
-		printf( "%i %i\n", i2, total_samples / 500 );
-		//Cleans the screen
-		glClear( GL_COLOR_BUFFER_BIT );
-		
-		//Plots the time axis.
-		glBegin( GL_LINES );
-			glColor3ub( 150, 150, 150 );
-			glVertex2f( i2*500, screenHeight / 2 );
-			glVertex2f( screenWidth + i2*500, screenHeight / 2 );
-		glEnd();
-		
-		//NOTE: The FFT algorithm which I've talked about here: 
-		// http://giuliom95.tumblr.com/post/134427522354/
-		// will be implemented in those two next cycles.
-		
-		//Plots the left channel in red. 
-		glBegin( GL_LINE_STRIP );
-			glColor3ub( 255, 100, 100 );
-			for( i = 0; i < total_samples; i+=WIDTH ) {
-				glVertex2f( i, screenHeight / 2 + left[i/WIDTH] * DEPTH );
-			} 	
-		glEnd();
+	//Closes the file.
+	fclose( input_file );
 	
-		//Plots the right channel in green.
-		glBegin( GL_LINE_STRIP );
-			glColor3ub( 100, 255, 100 );
-			for( i = 0; i < total_samples; i+=WIDTH ) {
-				glVertex2f( i, screenHeight / 2 + right[i/WIDTH] * DEPTH );
-			}	
-		glEnd();	
+	refresh_plot();
+	
+	//And now the plotting part.
+	short exit = 0;
+	while( !exit ) {
 		
-			
-		glMatrixMode( GL_PROJECTION );
-		glLoadIdentity();
-		glOrtho( i2*500, screenWidth + i2*500, screenHeight, 0.0, 1.0, -1.0 );
+		while( SDL_PollEvent( &e ) != 0 ) {
 		
-		SDL_GL_SwapWindow( gWindow );
+			if( e.type == SDL_QUIT ) {
+				exit = 1;
+			} else if( e.type == SDL_MOUSEWHEEL ) {
+				if( e.wheel.y != 0 ) {
+					position += e.wheel.y * SCROLL_SPEED;
+					refresh_plot();
+				}
+			} else if( e.type == SDL_KEYDOWN ) {
+				if( e.key.keysym.sym == SDLK_PLUS ) {
+					width++;
+					refresh_plot();
+				} else if( e.key.keysym.sym == SDLK_MINUS && width > 1 ) {
+					width--;
+					refresh_plot();
+				}
+			}
+		}
 		
-		SDL_Delay(20);
+		SDL_Delay(40);
 	}
 	
 	//Deallocates the memory allocated for the channels arrays.
 	free(left);
 	free(right);
-	
-	//Closes the file.
-	fclose( input_file );
 	
 	//Shuts down SDL on OpenGL things.
 	SDL_close();
